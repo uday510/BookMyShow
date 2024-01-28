8827723df11aa3bf924901f0b2978cbbca0f394c
Uday <budayteja009@gmail.com>
2024-01-28 20:26:28 +0530

    Add Models

---
+++ src/main/java/com/lld/bookmyshow/BookMyShowApplication.java
@@ -0,0 +1,15 @@
+package com.lld.bookmyshow;
+
+import com.lld.bookmyshow.models.BaseModel;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowApplication {
+
+	public static void main(String[] args) {
+		BaseModel baseModel = new BaseModel();
+
+		SpringApplication.run(BookMyShowApplication.class, args);
+	}
+}
---
+++ src/main/java/com/lld/bookmyshow/models/Movie.java
@@ -0,0 +1,9 @@
+package com.lld.bookmyshow.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Movie {
+}
